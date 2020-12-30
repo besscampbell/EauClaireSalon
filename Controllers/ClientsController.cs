fbb90263fff62343fb918e068e0de37e2bb22a39
Bess Campbell <bess.k.campbell@gmail.com>
2020-12-30 15:58:37 -0800

    Add create view to stylists

---
+++ Controllers/ClientsController.cs
@@ -0,0 +1,16 @@
+using HairSalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HairSalon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSalonContext _db;
+    public StylistsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+  }
+}
--- Models/Client.cs
+++ Models/Client.cs
@@ -6,7 +6,7 @@ namespace HairSalon.Models
   public class Client
   {
     public int ClientId { get; set; }
-    public string ClientName { get; set; }
+    public string Name { get; set; }
     public string PhoneNumber { get; set; }
     public int StylistId { get; set; }
     public virtual Stylist Stylist { get; set; }
