36b873fdd04e8cd49584d04685a530816dff9db1
Bess Campbell <bess.k.campbell@gmail.com>
2021-01-02 11:51:08 -0800

    Create paths in client controller

--- Controllers/ClientsController.cs
+++ Controllers/ClientsController.cs
@@ -1,5 +1,7 @@
 using HairSalon.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,10 +9,30 @@ namespace HairSalon.Controllers
 {
   public class ClientsController : Controller
   {
-    // private readonly HairSalonContext _db;
-    // public StylistsController(HairSalonContext db)
-    // {
-    //   _db = db;
-    // }
+    private readonly HairSalonContext _db;
+    public ClientsController(HairSalonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Name");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
